8b4e838828c0c2d4338ead1fcedc241f6bcc0df6
silas rodrigues <82327338@ulife.com.br>
2024-03-26 20:59:11 -0300

    apostila(git-js): funcao multiplicacao implementada

--- calc.js
+++ calc.js
@@ -1,6 +1,14 @@
 module.exports = {
     soma: (x, y) => x + y,
-    subtracao: function(x, y) {
+
+    subtracao: function (x, y) {
         return x - y
-    } 
+    },
+
+    multiplicacao: (x, y) => {
+        return x * y
+    },
+
+    
+
 }
---
+++ teste.js
@@ -0,0 +1,5 @@
+const calc = require('./calc.js');
+
+calc.soma(2, 3);
+calc.subtracao(2, 3);
+calc.multiplicacao(2, 3);
