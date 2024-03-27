4117ef232a518f34c2444467741dffe58d125d16
silas rodrigues <82327338@ulife.com.br>
2024-03-26 21:01:47 -0300

    apostila(git-js): funcoes implementadas

--- calc.js
+++ calc.js
@@ -9,6 +9,8 @@ module.exports = {
         return x * y
     },
 
-    
+    divisao: function(x, y) {
+        return x / y
+    }
 
 }
--- teste.js
+++ teste.js
@@ -3,3 +3,4 @@ const calc = require('./calc.js');
 calc.soma(2, 3);
 calc.subtracao(2, 3);
 calc.multiplicacao(2, 3);
+calc.divisao(2, 5);
